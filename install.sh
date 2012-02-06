1206bb1f20d18a5b1fe62662a1679dc5afb89275
Josh Dick <josh@joshdick.net>
2012-02-05 21:09:40 -0500

    Better checks for install script

--- install.sh
+++ install.sh
@@ -3,12 +3,15 @@
 # Install script for Josh Dick's dotfiles
 # <https://github.com/joshdick/dotfiles>
 
-for file in `find ${PWD} -maxdepth 1 -name \*.symlink`; do
-    src_file=`basename "$file"`
-    dest_file=`echo ".$src_file" | sed "s/\.symlink//g"`
-    if [ test -a $dest_file ]; then
-      echo "$dest_file already exists; skipping it..."
-    else
-      ln -sv `pwd`/$src_file ~/$dest_file
-    fi
+SELF=`readlink -f $0` # Path to this script
+SELF_PATH=`dirname $SELF` # Path to the directory containing this script
+
+for file in `find $SELF_PATH -maxdepth 1 -name \*.symlink`; do
+  src_file=`basename "$file"`
+  dest_file=`echo "$HOME/.$src_file" | sed "s/\.symlink$//g"`
+  if [ -e $dest_file ]; then
+    echo "$dest_file already exists; skipping it..."
+  else
+    ln -sv $SELF_PATH/$src_file $dest_file
+  fi
 done
