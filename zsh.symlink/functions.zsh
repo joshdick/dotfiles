5afbdfb3bb2cfc1927cac358aa1b67dc7945c6c8
Josh Dick <josh@joshdick.net>
2013-07-15 22:52:45 -0400

    functions.zsh: Added replacein() function

--- zsh.symlink/functions.zsh
+++ zsh.symlink/functions.zsh
@@ -42,6 +42,12 @@ function searchin() {
   fi
 }
 
+# Find and replace a string in all files recursively, starting from the current directory.
+# Adapted from code found at <http://forums.devshed.com/unix-help-35/unix-find-and-replace-text-within-all-files-within-a-146179.html>
+function replacein() {
+  find . -type f | xargs perl -pi -e "s/$1/$2/g"
+}
+
 # Serves the current directory over HTTP, on an optionally-specified port
 # If on Mac OS X, opens in the default browser
 function serve() {
