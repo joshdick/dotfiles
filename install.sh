8a60fa3995015090f5f2eab576104904f19edff9
Josh Dick <josh@joshdick.net>
2014-06-20 22:02:33 -0400

    Tweaked install script, added single line install via bootstrap script, updated README accordingly

--- install.sh
+++ install.sh
@@ -10,17 +10,26 @@ SELF_PATH="$( cd "$( dirname "$0" )" && pwd )" # Path to the directory containin
 
 # Create symlinks
 for file in `find $SELF_PATH -maxdepth 1 -name \*.symlink`; do
-  src_file=`basename "$file"`
-  dest_file=`echo "$HOME/.$src_file" | sed "s/\.symlink$//g"`
-  if [ -e $dest_file ]; then
-    echo "$dest_file already exists; skipping it..."
-  else
-    ln -sv $SELF_PATH/$src_file $dest_file
-  fi
+	src_file=`basename "$file"`
+	dest_file=`echo "$HOME/.$src_file" | sed "s/\.symlink$//g"`
+	if [ -e $dest_file ]; then
+		echo "$dest_file already exists; skipping it..."
+	else
+		ln -sv $SELF_PATH/$src_file $dest_file
+	fi
 done
 
 # Initialize Git submodules
-START_PATH=$PWD
-cd "$SELF_PATH"
-git submodule init && git submodule update
-cd "$START_PATH"
+git --work-tree "$SELF_PATH" submodule init
+
+if [ $? -ne 0 ]; then
+	echo "Error: There was a problem initializing the dotfiles repository submodules!" 1>&2;
+	exit 1
+fi
+
+git --work-tree "$SELF_PATH" submodule update
+
+if [ $? -ne 0 ]; then
+	echo "Error: There was a problem updating the dotfiles repository submodules!" 1>&2;
+	exit 1
+fi
