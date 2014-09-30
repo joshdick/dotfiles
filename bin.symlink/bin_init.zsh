6b9400f8ceb19bf83dabe3ed26896f622e61e6d1
Josh Dick <josh@joshdick.net>
2014-09-30 14:14:22 -0400

    Git submodules: Add hr, remove cdd, tweak bin_init.zsh

--- bin.symlink/bin_init.zsh
+++ bin.symlink/bin_init.zsh
@@ -4,30 +4,35 @@ PERSONAL_BIN=~/.bin
 # Add the personal bin to PATH
 export PATH=$PATH:$PERSONAL_BIN
 
+# Adds the given path (relative to personal bin) to PATH
+function addToPATH() {
+  export PATH=$PATH:$PERSONAL_BIN/$1
+}
+
 # Set up fasd <https://github.com/clvv/fasd>
-export PATH=$PATH:$PERSONAL_BIN/fasd
+addToPATH fasd
 eval "$(fasd --init auto)"
 
 # Set up resty <https://github.com/micha/resty>
 . $PERSONAL_BIN/resty/resty
 
 # set up formd
-export PATH=$PATH:$PERSONAL_BIN/formd
+addToPATH formd
 
 # Set up git-dude <https://github.com/sickill/git-dude>
-export PATH=$PATH:$PERSONAL_BIN/git-dude
+addToPATH git-dude
 
 # Set up git-cr <https://github.com/joshdick/git-cr>
-export PATH=$PATH:$PERSONAL_BIN/git-cr
+addToPATH git-cr
+
+# Set up hr <https://github.com/LuRsT/hr>
+addToPATH hr
 
 # Set up pytograph <https://github.com/joshdick/pytograph>
-export PATH=$PATH:$PERSONAL_BIN/pytograph
+addToPATH pytograph
 
 # Set up tmuxomatic <https://github.com/oxidane/tmuxomatic>
-export PATH=$PATH:$PERSONAL_BIN/tmuxomatic
-
-# Set up cdd <https://github.com/jestor/cdd>
-alias cdd="$PERSONAL_BIN/cdd/cdd.sh"
+addToPATH tmuxomatic
 
 # Set up fzf <https://github.com/junegunn/fzf>
 alias fzf="$PERSONAL_BIN/fzf/fzf"
