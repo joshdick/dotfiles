c3f1fef68184c38d487feb61f02f5381f455cd85
Josh Dick <josh@joshdick.net>
2017-05-12 16:13:33 -0400

    term.sh: Allow running from any directory

--- term/term.sh
+++ term/term.sh
@@ -3,6 +3,9 @@ set -ue
 
 # Installs necessary terminfos for tmux and iTerm2 on macOS
 
+# Path to this script
+SCRIPT_PATH=$(cd -P -- "$(dirname -- "$0")" && pwd -P)
+
 # Based on < https://apple.stackexchange.com/questions/249307/tic-doesnt-read-from-stdin-and-segfaults-when-adding-terminfo-to-support-italic#comment309243_249307 >
 mkdir -p ~/.terminfo
 export TERMINFO=~/.terminfo
@@ -10,4 +13,4 @@ curl http://invisible-island.net/datafiles/current/terminfo.src.gz | gunzip > "$
 tic -x -e tmux "$TERMINFO/terminfo.src"
 tic -x -e tmux-256color "$TERMINFO/terminfo.src"
 
-tic -x ./terminfo/iterm2.terminfo
+tic -x "$SCRIPT_PATH/terminfo/iterm2.terminfo"
