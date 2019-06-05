fe8a53b525bfdb0842941e88f301656f208273a0
Josh Dick <josh@joshdick.net>
2019-06-05 15:26:20 -0400

    update(): Fix accidental python indentation error

--- zsh.symlink/functions.zsh
+++ zsh.symlink/functions.zsh
@@ -363,7 +363,7 @@ import pkg_resources
 from subprocess import call
 
 packages = [dist.project_name for dist in pkg_resources.working_set]
-  call("pip install --upgrade " + ' '.join(packages), shell=True)
+call("pip install --upgrade " + ' '.join(packages), shell=True)
 END
     fi
   fi
