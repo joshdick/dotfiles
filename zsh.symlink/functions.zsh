0d90979d98c2569a82359e8817dd8632f96acf53
Josh Dick <josh@joshdick.net>
2019-11-06 22:57:49 -0500

    Add coc.nvim configuration and extensions

--- zsh.symlink/functions.zsh
+++ zsh.symlink/functions.zsh
@@ -346,6 +346,11 @@ function update() {
   heading "[vim] Updating Vim helptags..."
   vim '+helptags ALL' +qall
 
+  if command_exists yarn; then
+    heading "[coc] Updating coc.nvim and associated extensions..."
+    ls -d $DOTFILES_LOCATION/vim.symlink/pack/plugins/opt/coc* | xargs -L 1 zsh -c 'cd "$0" && yarn install --frozen-lockfile'
+  fi
+
   if command_exists npm; then
     heading "[npm] Updating global packages..."
     npm -g update
