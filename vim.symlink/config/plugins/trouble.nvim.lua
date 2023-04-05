f89fe18f3554bf9b03b26b22a941a458c2de9247
Josh Dick <josh@joshdick.net>
2023-04-05 19:39:52 -0400

    Remove seemingly unnecessary treesitter wrapper from trouble init

--- vim.symlink/config/plugins/trouble.nvim.lua
+++ vim.symlink/config/plugins/trouble.nvim.lua
@@ -1,14 +1,12 @@
 vim.cmd [[packadd trouble.nvim]]
 
-require'nvim-treesitter.configs'.setup {
-  require("trouble").setup {
-    auto_open = true,
-    auto_close = true,
-    mode = "document_diagnostics",
-    icons = false,
-    use_diagnostic_signs = true,
-    fold_open = "v",
-    fold_closed = "❯"
-  }
+require("trouble").setup {
+  auto_open = true,
+  auto_close = true,
+  mode = "document_diagnostics",
+  icons = false,
+  use_diagnostic_signs = true,
+  fold_open = "v",
+  fold_closed = "❯"
 }
 
