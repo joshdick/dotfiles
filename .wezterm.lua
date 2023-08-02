e55b5469c1446510c6ef5fd88b00f6b0c2cf50db
Josh Dick <josh@joshdick.net>
2023-08-02 02:53:48 -0400

    Update font size for MonoLisa font v2

--- .wezterm.lua
+++ .wezterm.lua
@@ -20,7 +20,7 @@ return {
     weight = 'Light',
     harfbuzz_features = { 'calt=0', 'clig=0', 'liga=0' },
   },
-  font_size = 15.75,
+  font_size = 15,
   -- cell_width = .9,
 
   window_frame = {
