b40794e89625a6a47f1ac5672b15b46284ec9bab
Josh Dick <josh@joshdick.net>
2024-05-10 12:06:46 -0400

    vim: switch from telescope.nvim to fzf-lua

---
+++ .vim/config/plugins/fzf-lua.lua
@@ -0,0 +1,22 @@
+vim.cmd [[packadd fzf-lua]]
+
+require("fzf-lua").setup {
+  "fzf-native"
+  -- hls = { border = "FloatBorder" }
+}
+
+--nnoremap <leader>g <cmd>Telescope live_grep<CR>
+vim.keymap.set("n", "<leader>g",
+  "<cmd>lua require('fzf-lua').live_grep()<CR>", { silent = true })
+
+--nnoremap <leader>cg :lua require('telescope.builtin').grep_string({ search = vim.fn.expand('<cword>')})<CR>
+
+--noremap <C-p> <cmd>Telescope find_files<CR>
+-- https://github.com/ibhagwan/fzf-lua/issues/881
+vim.keymap.set("n", "<C-p>",
+  "<cmd>lua require('fzf-lua').files({ fzf_opts = { ['--keep-right'] = '' } })<CR>", { silent = true })
+
+--noremap <C-b> <cmd>Telescope buffers<CR>
+vim.keymap.set("n", "<C-b>",
+  "<cmd>lua require('fzf-lua').buffers()<CR>", { silent = true })
+
