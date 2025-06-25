5799477a1ba3aa5a1c2293461268887d1f886dd1
Josh Dick <josh@joshdick.net>
2025-06-24 20:18:51 -0400

    trouble.nvim.lua: Update config

--- .vim/config/plugins/trouble.nvim.lua
+++ .vim/config/plugins/trouble.nvim.lua
@@ -3,10 +3,22 @@ vim.cmd [[packadd trouble.nvim]]
 require("trouble").setup {
   auto_open = false,
   auto_close = true,
-  mode = "document_diagnostics",
-  icons = false,
-  use_diagnostic_signs = true,
-  fold_open = "v",
-  fold_closed = "❯"
+  mode = "document_diagnostics", -- "workspace_diagnostics", "document_diagnostics", "quickfix", "lsp_references", "loclist"
+  icons = {
+    indent = {
+      top           = "│ ",
+      middle        = "├╴",
+      -- last          = "└╴",
+      -- last          = "-╴",
+      last       = "╰╴", -- rounded
+      fold_open     = "▽ ",
+      fold_closed   = "▷ ",
+      ws            = " ",
+    },
+    folder_closed   = "⊖ ",
+    folder_open     = "⊕ ",
+    },
 }
 
+vim.keymap.set("n", "<leader>L",
+  "<cmd>Trouble diagnostics toggle focus=false filter.buf=0<CR>", { silent = true })
