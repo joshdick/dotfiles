e5c9d8e828549df59f01b6254a52d74d87dce577
Josh Dick <josh@joshdick.net>
2026-04-07 12:02:42 -0400

    diffview.nvim.lua config: Disable `use_icons`

--- .vim/config/plugins/diffview.nvim.lua
+++ .vim/config/plugins/diffview.nvim.lua
@@ -1,4 +1,8 @@
 vim.cmd [[packadd diffview.nvim]]
 
+require("diffview").setup({
+  use_icons = false
+})
+
 vim.keymap.set('n', '<leader>D', '<cmd>DiffviewOpen<CR>', { noremap=true, silent=true })
 vim.keymap.set('n', '<leader>H', '<cmd>DiffviewFileHistory<CR>', { noremap=true, silent=true })
